bf88ca3789967d64b99bc63d035e115740991c38
Mihail-Makei <mihasiknet@phystech.edu>
2020-10-27 01:51:03 +0300

    Fixed

--- substr.cpp
+++ substr.cpp
@@ -3,25 +3,21 @@
 #include <string>
 
 
-void prefix_substr(const std::string& substr, std::vector<int>& substr_prefix) {
-    substr_prefix[0] = 0;
+std::vector<int> PrefixFunc(const std::string& substr) {
+    std::vector<int> prefix(substr.length(), 0);
+    prefix[0] = 0;
     for (int i = 1; i < substr.size(); ++i) {
-        int match = substr_prefix[i - 1];
+        int match = prefix[i - 1];
 
         while ((match != 0) && (substr[match] != substr[i])) {
-            match = substr_prefix[match - 1];
+            match = prefix[match - 1];
         }
 
-        substr_prefix[i] = (match == 0) ? substr[0] == substr[i] : match + 1;
-        if (match == 0)
-            substr_prefix[i] = (substr[0] == substr[i]);
-        else
-            substr_prefix[i] = match + 1;
+        prefix[i] = (match == 0) ? substr[0] == substr[i] : match + 1;
     }
-
 }
 
-int find_prefix(const char letter, const int position, const std::string& substr, std::vector<int>& prefix_values, int& last) {
+int FindPrefix(const char letter, const int position, const std::string& substr, std::vector<int>& prefix_values, int& last) {
     if (position == 0) {
         last = (substr[0] == letter);
         return last;
@@ -55,11 +51,9 @@ int main() {
     std::cin >> str;
 
     int last = 0;
-    std::vector<int> prefix_values(substr.size(), 0);
-
-    prefix_substr(substr, prefix_values);
+    std::vector<int> prefix_values = PrefixFunc(substr);
 
     for (int i = 0; i < str.size(); ++i)
-        if (find_prefix(str[i], i, substr, prefix_values, last) == substr.size())
+        if (FindPrefix(str[i], i, substr, prefix_values, last) == substr.size())
             std::cout << i + 1 - substr.size() << " ";
 }
