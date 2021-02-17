357e427468f35f7aad6af8061d99a1c86f26a83f
Mihail-Makei <mihasiknet@phystech.edu>
2021-02-17 21:55:36 +0300

    CamelCase

--- C-string-basement.cpp
+++ C-string-basement.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <vector>
 
-std::vector<int> prefix_function(const std::string& str) {
+std::vector<int> PrefixFunction(const std::string& str) {
     std::vector<int> prefix_func(str.length());
     prefix_func[0] = 0;
 
@@ -27,7 +27,7 @@ int main() {
     std::string str;
     std::cin >> str;
 
-    std::vector<int> prefix_func = prefix_function(str);
+    std::vector<int> prefix_func = PrefixFunction(str);
 
     std::cout << str.length() - prefix_func[str.length() - 1] << std::endl;
 
